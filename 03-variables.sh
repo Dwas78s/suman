308c54989794cb698fe1d58374b45a5acc2b4295
Dwas78s <sumansivala02@gmail.com>
2024-04-21 09:34:59 +0530

    03-variables(1)

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1:: Hello $PERSON2, How are you"
+echo "$PERSON2:: HI $PERSON1, I am fine. HOw are you?"
+echo "$PERSON1: I am fine too. How is your work?"
+echo "$PERSON2:: Not bad. Iam thinking to learn Devops"
