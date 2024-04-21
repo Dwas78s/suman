2b8085cc001d7218096e6af9d0d076c00dfa1e4e
Dwas78s <sumansivala02@gmail.com>
2024-04-21 09:40:18 +0530

    04-Variables

---
+++ 04-Variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2, How are you"
+echo "$PERSON2:: HI $PERSON1, I am fine. HOw are you?"
+echo "$PERSON1: I am fine too. How is your work?"
+echo "$PERSON2:: Not bad. Iam thinking to learn Devops"
