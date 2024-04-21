f5c3bc37fa94130d82b020f7f4555ae220d77eba
Dwas78s <sumansivala02@gmail.com>
2024-04-21 09:16:56 +0530

    Variables

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:: Hello Suresh, How are you
+echo "Suresh:: HI Ramesh, I am fine. HOw are you?"
+echo "Ramesh: I am fine too. How is your work?"
+echo "suresh:: Not bad. Iam thinking to learn Devops"
