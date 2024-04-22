817c3056ae592758f42542331752e39e44a1a192
Dwas78s <sumansivala02@gmail.com>
2024-04-22 19:34:34 +0530

    special-Variable

--- 08-special-character.sh
+++ 08-special-character.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "All variables:$@"
 echo "Number of variables passed:$#" 
-echo "scriptName:$0"
+echo "scriptName:$0":
+echo "current working Directory:$PWD"
+echo "Home Directory of current user:$Home"
+echo "which user is running the script:$User"
+echo "which process id is running:$$"
