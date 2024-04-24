622a9f1320a8a777991230a3f5d8995ca8ddc495
Dwas78s <sumansivala02@gmail.com>
2024-04-25 00:01:05 +0530

    Frontend

---
+++ 18-delete-logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+SOURCE_DIRECTORY= /tmp/app-logs
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+if [ -d $SOURCE_DIRECTORY ]
+then
+echo -e "$G source directory exists $N
+else
+echo -e "$R please make sure $SOURCE_DIRECTORY exists $n
+exit 1
+fi
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+while IFS= read -r line
+do 
+echo "Deleting file:$line"
+rm -rf $line
+done <<<$FILES
+ 
